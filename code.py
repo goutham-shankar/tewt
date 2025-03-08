d5ae47a1483648ed2f42be8142595cc581e45ca5
GOUTHAM SANKAR <84023210+goutham-shankar@users.noreply.github.com>
2025-03-08 10:02:16 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)  # GPIO 2 (Built-in LED on some ESP32 boards)
+
+while True:
+    led.value(1)  # Turn LED ON
+    time.sleep(1)  # Wait 1 second
+    led.value(0)  # Turn LED OFF
+    time.sleep(1)  # Wait 1 second
+
