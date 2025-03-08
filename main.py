467c6d661250c16a825df70f21da9a9525eb9f6a
GOUTHAM SANKAR <84023210+goutham-shankar@users.noreply.github.com>
2025-03-08 11:01:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ led = machine.Pin(2, machine.Pin.OUT)  # GPIO 2 (Built-in LED on some ESP32 boar
 
 while True:
     led.value(1)  # Turn LED ON
-    time.sleep(10)  # Wait 1 second
+    time.sleep(1)  # Wait 1 second
     led.value(0)  # Turn LED OFF
-    time.sleep(10)  # Wait 1 second
+    time.sleep(1)  # Wait 1 second
 
